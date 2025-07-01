f1eea28459fb3fcb1eb9100ab2c15d08799c70cd
Adel <zairiade34l@gmail.com>
2025-07-01 19:48:32 +0200

    Remove old folder with spaces

--- Projet - BlobIA/graph_pipeline.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def run():
-    subprocess.run(['python', 'graph_builder/normalize.py'], check=True)
-    subprocess.run(['python', 'graph_builder/build_graph.py'], check=True)
-
-if __name__ == '__main__':
-    run()
